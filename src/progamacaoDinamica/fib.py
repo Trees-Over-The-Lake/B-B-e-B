ca9ef9f7a773195496f87e1b4f68f4f884197aaa
Lucas Santiago <lu.santi.oli@gmail.com>
2021-11-27 13:02:19 -0300

    feat: codigo de programação dinamica finalizado

--- src/progamacaoDinamica/fib.py
+++ src/progamacaoDinamica/fib.py
@@ -1,3 +1,25 @@
+# Código baseado na implementação da free code camp
+# Referência: https://youtu.be/oBt53YbR9Kk?t=210
+
+import time
+
 # Código do fibonacci
+def fib(numero):
+    if (numero <= 2):
+        return 1
+    
+    return fib(numero-1) + fib(numero-2)
 
 # Versão do código usando programação dinâmica com memoization
+def fibMemo(numero, memo={}):     
+    if (numero in memo):
+        return memo[numero]
+    
+    if (numero <= 2):
+        return 1
+    
+    memo[numero] = fibMemo(numero - 1, memo) + fibMemo(numero - 2, memo)
+    return memo[numero]
+
+print(fib(35))
+print(fibMemo(35))
